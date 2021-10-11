1d68942e51d552305d5b9eccebc484ab46f91360
sefhirot69 <josalillo@gmail.com>
2021-10-11 09:40:48 +0200

    feat : Fetching data & the repository

--- start/src/Controller/QuestionController.php
+++ start/src/Controller/QuestionController.php
@@ -66,11 +66,19 @@ EOF
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper): Response
+    public function show(string $slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager): Response
     {
 
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
+        }
+
+        $repository = $entityManager->getRepository(Question::class);
+        /** @var Question|null $question */
+        $question = $repository->findOneBy(['slug' => $slug]);
+
+        if (! $question) {
+            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
         }
 
         $answers      = [

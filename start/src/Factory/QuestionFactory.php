cbbc8244c5a38af0f4218351ec317de545decd24
sefhirot69 <josalillo@gmail.com>
2021-10-14 10:27:49 +0200

    feat : doctrine extensions. Sluggable

--- start/src/Factory/QuestionFactory.php
+++ start/src/Factory/QuestionFactory.php
@@ -58,12 +58,7 @@ final class QuestionFactory extends ModelFactory
     {
         // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
         return $this
-             ->afterInstantiate(function(Question $question) {
-                 if(!$question->getSlug()){
-                     $slugger = new AsciiSlugger();
-                     $question->setSlug($slugger->slug($question->getName()));
-                 }
-             })
+//             ->afterInstantiate(function(Question $question) {})
         ;
     }
 
